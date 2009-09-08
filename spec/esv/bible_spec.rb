8ec71c9561bcd46d37cde47ac3fee02a9c5346a6
Geoffrey Dagley <gdagley@gmail.com>
2009-09-08 09:53:03 -0500

    adding stubs for Bible specs

---
+++ spec/esv/bible_spec.rb
@@ -0,0 +1,22 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ESV::Bible do
+  before do
+    @bible = ESV::Bible.new(:key => 'TEST')
+  end
+  
+  describe "passage_query" do
+  end
+  
+  describe "query" do
+  end
+  
+  describe "query_info" do
+  end
+  
+  describe "verse" do
+  end
+
+  describe "daily_verse" do
+  end
+end
